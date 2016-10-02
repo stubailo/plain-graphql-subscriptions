aad0f5c0e8e0fa91fdc5c287fc6edf1b52bad2cc
Sashko Stubailo <sashko@stubailo.com>
2016-10-01 21:46:43 -0700

    Add minimal example

---
+++ plain.js
@@ -0,0 +1,46 @@
+import { PubSub, SubscriptionManager } from 'graphql-subscriptions';
+import { makeExecutableSchema } from 'graphql-tools';
+
+const messages = [];
+
+const resolvers = {
+  Query: { messages: () => messages },
+  Subscription: { newMessage: (rootValue) => rootValue },
+};
+
+const typeDefs = `
+type Query {
+  messages: [String!]!
+}
+type Subscription {
+  newMessage: String!
+}
+`;
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+const pubsub = new PubSub();
+const subscriptionManager = new SubscriptionManager({
+  schema,
+  pubsub,
+});
+
+subscriptionManager.subscribe({
+  query: `
+    subscription NewMessageSubscription {
+      newMessage
+    }
+  `,
+  callback: (err, result) =>
+    console.log(`New message: ${result.data.newMessage}`),
+});
+
+const helloWorldMessage = 'Hello, world!';
+messages.push(helloWorldMessage);
+
+pubsub.publish('newMessage', helloWorldMessage);
+
+// Prints "New message: Hello, world!" in the console via the subscription!
